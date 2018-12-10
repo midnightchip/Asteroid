bc6f6f041f4398690c246cbcd36dc07837d49ab4
MidnightChip <midnightchips@gmail.com>
2018-12-09 22:45:50 -0700

    Prefs, legal

--- preferences/LWPPreferenceController.m
+++ preferences/LWPPreferenceController.m
@@ -32,6 +32,27 @@
     NSArray *unorganized =  [[NSArray alloc] initWithObjects: @"SevereThunderstorm",@"Rain", @"Thunderstorm", @"Haze", @"PartlyCloudyDay", @"MixedRainAndSnow", @"SnowFlurries", @"Smoky", @"MixedRainAndSleet", @"ClearNight", @"SnowShowers", @"MixedSnowAndSleet", @"Breezy", @"ScatteredSnowShowers", @"FreezingDrizzle", @"BlowingSnow", @"Sunny", @"Drizzle", @"Windy", @"MostlySunnyNight", @"Snow", @"HeavySnow", @"Frigid", @"ScatteredSnowShowers", @"MostlySunnyDay", @"Hail", @"Blizzard", @"Cloudy", @"MixedRainFall", @"Sleet", @"PartlyCloudyDay", @"MostlyCloudyNight", @"Hot", @"Dust", @"HeavyRain", @"MostlyCloudyDay", @"IsolatedThunderstorms", @"SnowShowers", @"PartlyCloudyNight", @"ScatteredShowers", @"IsolatedThundershowers", @"ScatteredThunderstorms", @"Tornado", @"FreezingRain", @"TropicalStorm", @"Showers1", @"Hurricane", @"Fog", nil];
     return [unorganized sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
 }
+-(void)gplInfo{
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"License Information"
+                           message: @"Asteroid is Licensed under the GPL 3.0 License. A Copy is included in the preference bundle of this tweak, along with my github page."
+                           preferredStyle:UIAlertControllerStyleAlert];
+
+    UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+                               handler:^(UIAlertAction * action) {}];
+
+    UIAlertAction* read = [UIAlertAction actionWithTitle:@"Let Me See it" style:UIAlertActionStyleDefault
+                               handler:^(UIAlertAction * action) {
+                                   [self openGPL];
+                                   }];
+
+    [alert addAction:defaultAction];
+    [alert addAction:read];
+    [self presentViewController:alert animated:YES completion:nil];
+}
+-(void)openGPL{
+    NSURL *url = [NSURL URLWithString:@"https://github.com/midnightchip/Asteroid/blob/master/LICENSE.md"];
+    [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+}
 @end 
 
 @implementation LWPPreferenceController
